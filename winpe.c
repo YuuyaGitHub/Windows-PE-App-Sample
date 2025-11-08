4de4b63fe12838c2df667b984dd58233a2bc01a0
Yuuya <83148287+YuuyaGitHub@users.noreply.github.com>
2025-11-08 20:10:53 +0900

    Add files via upload

---
+++ winpe.c
@@ -0,0 +1,30 @@
+#define UNICODE
+#define _UNICODE
+#include <windows.h>
+
+#pragma comment(lib, "kernel32.lib")
+#pragma comment(lib, "advapi32.lib")
+
+void __stdcall mainCRTStartup(void) {
+    // WinPEîªíË
+    HKEY hKey;
+    LONG r = RegOpenKeyExW(
+        HKEY_LOCAL_MACHINE,
+        L"SYSTEM\\CurrentControlSet\\Control\\MiniNT",
+        0,
+        KEY_READ,
+        &hKey
+    );
+
+    if (r != ERROR_SUCCESS) {
+        ExitProcess(0);    // í èÌWindowsÇ»ÇÁë¶èIóπ
+    }
+    RegCloseKey(hKey);
+
+    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+    const wchar_t msg[] = L"Hello, Windows PE!\r\n";
+    DWORD written;
+    WriteConsoleW(h, msg, sizeof(msg) / sizeof(msg[0]) - 1, &written, NULL);
+
+    ExitProcess(0);
+}
